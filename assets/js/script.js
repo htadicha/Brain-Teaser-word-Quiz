b88c3c8a3d31475b6ee68aa117e264760b9a730f
Hussein `Adan Tadicha <husseinadantadicha@Fadhumos-MacBook-Air.local>
2024-04-21 12:51:09 +0100

    function for timer and random selection of word defiined using fisher-yates algo

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,18 +8,31 @@ document.addEventListener("DOMContentLoaded", function () {
         inputField = document.getElementsByTagName("input")[0],
         refreshBtn = document.getElementsByClassName("refresh")[0],
         checkBtn = document.getElementsByClassName("check")[0];
+    // Credit: timer function taken and edited from 
+    // https://teamtreehouse.com/community/how-do-i-set-up-setinterval-clearinterval-settimeout-and-cleartimeout-properly-alsodo-i-need-to-use-all-4
     let correctWord, timer;
     let initTimer = MaxTime => {
-       clearInterval(timer);
-       timer = setInterval(() => {
-        if(MaxTime > 0){
-            MaxTime--;
-            timeText.innerText = MaxTime;
-        } else {
         clearInterval(timer);
-        alert (`Time off! ${correctWord.toUppercase()} was the correct word`);
-        initGame();
-     }
-       }, 1000); 
+        timer = setInterval(() => {
+            if (MaxTime > 0) {
+                MaxTime--;
+                timeText.innerText = MaxTime;
+            } else {
+                clearInterval(timer);
+                alert(`Time off! ${correctWord.toUppercase()} was the correct word`);
+                initGame();
+            }
+        }, 1000);
     };
+
+    // game function taken and edited from the training materials
+    let initGame = () => {
+        initTimer(30);
+        let randomObj = wordslist[Math.floor(math.random() * wordslist.length)];
+        let wordArray = randomObj.word.split("");
+        for (let i = wordArray.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+        }
+    }
 })

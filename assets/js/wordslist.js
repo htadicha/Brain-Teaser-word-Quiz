0676aee8e66914fd0496cfa653133ab6af3680dd
Hussein `Adan Tadicha <husseinadantadicha@Fadhumos-MacBook-Air.local>
2024-04-21 08:34:56 +0100

    adding placeholder wordlist to wordlist.js

---
+++ assets/js/wordslist.js
@@ -0,0 +1,22 @@
+let Words = [
+    {
+        word: "latitude",
+        hint: "Angular distance north or south from the equator"
+    },
+    {
+        word: "latitude",
+        hint: "Angular distance north or south from the equator"
+    },
+    {
+        word: "latitude",
+        hint: "Angular distance north or south from the equator"
+    },
+    {
+        word: "latitude",
+        hint: "Angular distance north or south from the equator"
+    },
+    {
+        word: "latitude",
+        hint: "Angular distance north or south from the equator"
+    }
+];

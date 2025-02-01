6ff7296901f6d3a9960409c71d84f03776218955
Hussein Tadicha <husseintadicha@Cexs-MacBook-Pro.local>
2025-02-01 12:48:28 +0000

    updated JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,10 +89,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 clearInterval(timer);
 
                 // Ask the user if they want to continue
-                let userChoice = confirm(" ⌛ Time's up while idle! Do you want to continue playing");
+                let userChoice = confirm("Time's up! Do you want to continue to the next word?");
                 if (userChoice) {
                     // Record it as a wrong answer and move to the next word
-                    showFeedback("⌛ No trial miss! Moving to the next word.", false);
+                    showFeedback("⌛ Time's up! Moving to the next word.", false);
                     trialCount++; // Increment trial count
                     setTimeout(() => {
                         feedbackText.style.display = "none"; // Hide feedback
